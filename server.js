d5d45c0f9f1ba3f6cb1d712c71664defbf36cb5b
jkosturko <jessica.kosturko@gmail.com>
2015-06-20 10:44:20 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+    socket.on('click', function (data) {
+    console.log('myclick', data);
+  });
+});
